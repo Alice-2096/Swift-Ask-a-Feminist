8188928dddde4ffa45f7b6cdc04f7bd85196a973
Alice <xinranjiang2015@yahoo.com>
2022-11-09 09:48:34 -0500

    Initial Commit

---
+++ ask-a-feminist/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ask-a-feminist
+//
+//  Created by apple on 2022/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, SwiftUI!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ask-a-feminist/ask_a_feministApp.swift
@@ -0,0 +1,17 @@
+//
+//  ask_a_feministApp.swift
+//  ask-a-feminist
+//
+//  Created by apple on 2022/11/9.
+//
+
+import SwiftUI
+
+@main
+struct ask_a_feministApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
